0952c7b9a148dae80c7d3726aa0301253d3251ad
cuongbq <erikbuidt@gmail.com>
2025-04-24 16:13:31 +0700

    fix legacy compoment

--- src/components/custom/nav-bar.tsx
+++ src/components/custom/nav-bar.tsx
@@ -95,7 +95,7 @@ export default function NavBar() {
           </NavigationMenuContent>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <Link href="/docs" legacyBehavior passHref>
+          <Link href="/docs" passHref>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
               Documentation
             </NavigationMenuLink>
